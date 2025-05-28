43aa2759edb6b748a4046e397fad0f465f848150
pablorjd1 <pablo.jimenez2221@gmail.com>
2025-05-27 22:52:30 -0400

    Se incorpora libreria de crashlitycs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.service)
+    alias(libs.plugins.crashlytics)
 }
 
 android {
